f6582421e1928371c268d0cb6904b995b9963c35
pikesley <github@orgraphone.org>
2017-10-15 20:28:17 +0100

    Kind of works

--- pixley.py
+++ pixley.py
@@ -15,3 +15,31 @@ def light(neopixels, pixels, colour):
 
     for p in pixels:
         neopixels[p % neopixels.n] = colour
+
+class WavePosition:
+    position = 0
+
+def wave(neopixels, colour):
+    light(neopixels, (0, 11), fractional_colour(colour, 0.25))
+    light(neopixels, [WavePosition.position, neopixels.n - (WavePosition.position + 1)], colour)
+
+    WavePosition.position = WavePosition.position + 1
+
+def fractional_colour(colour, proportion):
+    return tuple(map(lambda x: int(x * proportion), colour))
+
+# def wheel(pos):
+#     # Input a value 0 to 255 to get a color value.
+#     # The colours are a transition r - g - b - back to r.
+#     if (pos < 0):
+#         return (0, 0, 0)
+#     if (pos > 255):
+#         return (0, 0, 0)
+#     if (pos < 85):
+#         return (int(pos * 3), int(255 - (pos*3)), 0)
+#     elif (pos < 170):
+#         pos -= 85
+#         return (int(255 - pos*3), 0, int(pos*3))
+#     else:
+#         pos -= 170
+#         return (0, int(pos*3), int(255 - pos*3))
