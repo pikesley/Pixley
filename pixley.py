2f44fe000e4c482c79b1c83e90d32822f181c38c
pikesley <github@orgraphone.org>
2017-10-19 22:41:47 +0100

    About to wreck this

--- pixley.py
+++ pixley.py
@@ -38,6 +38,35 @@ def wave(neopixels, colour):
 def fractional_colour(colour, proportion):
     return tuple(map(lambda x: int(x * proportion), colour))
 
+def knight_rider_sequence(count):
+    offset = 4
+
+    tops = []
+    for i in range(count / 4):
+        tops.append(i + offset)
+
+    bottoms = []
+    for i in range(count / 4, 0, -1):
+        v = i - offset - 1
+        if v < 0:
+            v = (count / 2) + v
+        bottoms.append(v)
+
+    for i in range(count / 4):
+        tops.append(i + offset + (count / 2))
+
+    for i in range(count / 4, 0, -1):
+        v = (i - offset - 1) + count / 2
+        if v < count / 2:
+            v = (count / 2) + v
+        bottoms.append(v)
+
+    seq = []
+    for i in range(count / 2):
+        seq.append((tops[i], bottoms[i]))
+
+    return seq
+
 # def wheel(pos):
 #     # Input a value 0 to 255 to get a color value.
 #     # The colours are a transition r - g - b - back to r.
